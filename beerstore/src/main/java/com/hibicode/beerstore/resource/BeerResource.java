d21d15c165ea538912237fc53b987fba51f7dc72
Wilton Junior <wiltonjunior@unicatolicaquixada.edu.br>
2018-10-31 12:47:43 -0300

    Implementação de cadastro e listagem

--- beerstore/src/main/java/com/hibicode/beerstore/resource/BeerResource.java
+++ beerstore/src/main/java/com/hibicode/beerstore/resource/BeerResource.java
@@ -1,20 +1,35 @@
 package com.hibicode.beerstore.resource;
 
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.hibicode.beerstore.model.Beer;
+import com.hibicode.beerstore.repository.Beers;
 
 @RestController
 @RequestMapping("/beers")
 public class BeerResource {
 
+	@Autowired
+	private Beers beers;
+	
 	@GetMapping
-	public List<String> all() {
-		return Arrays.asList("Heineken", "Colorado Indigo", "Stella Artois");
+	public List<Beer> all() {
+		return beers.findAll();
 	}
 	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Beer create(@RequestBody Beer beer) {
+		return beers.save(beer);
+	}
 }
